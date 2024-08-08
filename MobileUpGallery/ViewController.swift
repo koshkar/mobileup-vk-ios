2ca48b7d5e2335f96fc64b4ccd286b529fe407b8
koshkar <qwerty376l@mail.ru>
2024-08-08 17:15:31 +0300

    Add base structure

---
+++ MobileUpGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileUpGallery
+//
+//  Created by Кирилл Кошкарёв on 08.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
