a6354a62a135df71d2fbb21855e6c5705a1378b9
koshkar <qwerty376l@mail.ru>
2024-08-08 20:44:58 +0300

    Add base main view & work on vk sign in

---
+++ MobileUpGallery/Controllers/ViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let mainView = MainView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Добавление действия для кнопки
+        mainView.button.addTarget(self, action: #selector(openOAuthWebView), for: .touchUpInside)
+    }
+    
+    @objc func openOAuthWebView() {
+        let oAuthViewController = OAuthViewController()
+        oAuthViewController.modalPresentationStyle = .formSheet
+        present(oAuthViewController, animated: true, completion: nil)
+    }
+}
